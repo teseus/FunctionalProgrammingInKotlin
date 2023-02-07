4cd8f3cf2365c6d6be4e382d90f927fdc787e640
hyuksu.pyun <hyuksu.pyun@kurlycorp.com>
2023-02-07 13:14:37 +0900

    Initial commit for a book "Functional Programming In Kotlin"

---
+++ src/main/kotlin/com/teseus/functionalprogramminginkotlin/FunctionalProgrammingInKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.teseus.functionalprogramminginkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FunctionalProgrammingInKotlinApplication
+
+fun main(args: Array<String>) {
+    runApplication<FunctionalProgrammingInKotlinApplication>(*args)
+}
